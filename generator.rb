e33296ea7cb5ef3bcce528902245de7fe6033ace
Dejian Xu <xudejian2008@gmail.com>
2013-07-31 13:16:54 +0800

    dump 0.1

--- generator.rb
+++ generator.rb
@@ -1,8 +1,6 @@
 #plugins:
 #  sitemap:
 #    file_name: sitemap.xml
-#    exclude_id:
-#      - search.html
 
 require 'nokogiri'
 
@@ -11,35 +9,66 @@ class Ruhoh
     class SitemapTask
       def initialize(ruhoh)
         @ruhoh = ruhoh
+        load_site_sitemap
       end
 
       def run
-        production_url = @ruhoh.config['production_url']
-        config = @ruhoh.config['plugins']['sitemap'] || {} rescue {}
-        sitemap_file_name = config['file_name'] || "sitemap.xml"
-        exclude_id = config['exclude_id'] || []
-
         sitemap = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
           xml.urlset(:xmlns => "http://www.sitemaps.org/schemas/sitemap/0.9") {
-            @ruhoh.collection(:posts).all.each do |post|
-              p post
-              exit
-              xml.url {
-                xml.loc_ "#{production_url}#{post['url']}"
-                xml.lastmod_ File.mtime(post.)
-                xml.priority_ post[:priority] if !!post[:priority]
-                xml.changefreq_ post[:changefreq] if !!post[:changefreq]
-              }
-            end #posts
+            @ruhoh.collections.acting_as_pages.each do |name|
+              collection = @ruhoh.collection(name)
+              next unless collection.compiler?
+              add_to_sitemap(xml, collection)
+            end
           }
         end
 
         FileUtils.cd(@ruhoh.paths.compiled) {
+          sitemap_file_name = @config['file_name'] || "sitemap.xml"
           File.open(sitemap_file_name, 'w'){ |p| p.puts sitemap.to_xml }
 
           Ruhoh::Friend.say { green "  > #{sitemap_file_name}" }
         }
       end
+
+      protected
+
+      def add_to_sitemap(xml, collection)
+        production_url = @ruhoh.config['production_url']
+        update_page_sitemap collection
+
+        collection.all.each do |page|
+          xml.url {
+            xml.loc_ "#{production_url}#{page['url']}"
+            xml.lastmod_ File.mtime(page.pointer['realpath'])
+            xml.priority_ page.data['priority']
+            xml.changefreq_ page.data['changefreq']
+          }
+        end
+      end
+
+      def collection_sitemap(collection)
+        @config.merge(collection.config['sitemap'] || {})
+      end
+
+      def get_page_default_sitemap(collection)
+        sitemap = collection_sitemap collection
+        Hash[ ['changefreq', 'priority'].map {|d| [d, sitemap[d]]} ]
+      end
+
+      def update_page_sitemap(collection)
+        default_config = get_page_default_sitemap collection
+        collection.all.each do |page|
+          page.data.merge!(default_config) { |key, vold| vold }
+        end
+      end
+
+      def load_site_sitemap
+        default_config = {'changefreq' => 'daily', 'priority' => 0.5}
+        @config = @ruhoh.config['plugins']['sitemap'] || {} rescue {}
+        @config.merge!(default_config){ |key, vold| vold }
+      end
+
     end
   end
 end
